ac832d952f95a2bfbe5633418a23cebc62dfacff
Vladimir Zaytsev <vladimir@zvm.me>
2012-02-21 18:09:25 +0600

    added support of Facebook shares and likes

--- distr/sn/worker.py
+++ distr/sn/worker.py
@@ -48,11 +48,12 @@ class WkSNDataFetcher(WkTextDataFetcher):
 			response_raw.li = li_raw
 			response_raw.dg = dg_raw
 			# store data
-			response.fb = json.loads(fb_raw).get("shares", 0)
+			response.fb_shr = json.loads(fb_raw).get("shares", 0)
+			response.fb_com = json.loads(fb_raw).get("comments", 0)
 			response.tw = json.loads(tw_raw).get("count", 0)
 #			if tp_raw:
-#				response.t1 = json.loads(tp_raw).get("response",{"all":0}).get("all", 0)
-#				response.t2 = json.loads(tp_raw).get("response",{"influential":0}).get("influential",0)
+#				response.tp_all = json.loads(tp_raw).get("response",{"all":0}).get("all", 0)
+#				response.tp_inf = json.loads(tp_raw).get("response",{"influential":0}).get("influential",0)
 			response.su = int(json.loads(su_raw).get("result", {"views":0}).get("views", 0))
 			response.li = json.loads(li_raw[26:len(li_raw)]).get("count",0)
 			response.dg = json.loads(dg_raw[19:-2]).get("diggs",0)
