6efe8b5dbc28b30cd38ec3865bd76fc39efe1f7c
Vladimir Zaytsev <vladimir@zvm.me>
2012-02-27 12:33:50 +0600

    added POS tagging feature to lexicon builder

--- analyzer/lexicon.py
+++ analyzer/lexicon.py
@@ -6,24 +6,42 @@
 # and document frequencies.
 
 from options import DATABASE_CONFIG
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Table
 from sqlalchemy.types import Integer, String, Float
 from sqlalchemy.engine.url import URL
 from sqlalchemy import Column, func
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from analyzer.nlp import tokenize
+from analyzer.nlp import make_tokenizer, assign_tags
 
 import logging
 import sys
 
 DEFAULT_TEXT_FIELDS = ("title", "short_text", "full_text",)
 
+db_base = declarative_base()
+class LexiconTerm(db_base):
+	__tablename__ = "set_lexicon"				# default lexicon table
+	id = Column(Integer, primary_key=True)		# lexicon term id
+	tag = Column(String)						# term speech tag
+	term = Column(String)						# term string
+	tfreq = Column(Integer)						# total terms frequency
+	dfreq = Column(Integer)						# term document frequency
+	rfreq = Column(Float)						# relative document frequency
+	def __init__(self, term, tag):
+		self.term = term
+		self.tag = tag
+		self.tfreq = 0
+		self.dfreq = 0
+		self.rfreq = 0.0
+	def __repr__(self):
+		return "<LexiconTerm('{0}')>".format(self.term)
+
 
 def lexicon(input_table_name="set_input",
 			lexicon_table_name="set_lexicon",
 			text_fields=DEFAULT_TEXT_FIELDS,
-			csv_output="/tmp/set_lexicon.csv",
+			#csv_output="/tmp/set_lexicon.csv",
 			verbose=True):
 	"""
 	This function takes documents from input table,
@@ -46,23 +64,9 @@ def lexicon(input_table_name="set_input",
 		host=DATABASE_CONFIG["repository"]["host"],
 		port=DATABASE_CONFIG["repository"]["port"],
 		database=DATABASE_CONFIG["repository"]["database"],)
-	db_base = declarative_base()
 	db_engine = create_engine(db_url)
 	db_session = sessionmaker(bind=db_engine, autoflush=False)()
-	class LexiconTerm(db_base):
-		__tablename__ = lexicon_table_name
-		id = Column(Integer, primary_key=True)		# lexicon term id
-		term = Column(String)						# term string
-		tfreq = Column(Integer)						# total terms frequency
-		dfreq = Column(Integer)						# term document frequency
-		rfreq = Column(Float)						# relative document frequency
-		def __init__(self, term):
-			self.term = term
-			self.tfreq = 0
-			self.dfreq = 0
-			self.rfreq = 0.0
-		def __repr__(self):
-			return "<LexiconTerm('{0}')>".format(self.term)
+	LexiconTerm.__tablename__ = lexicon_table_name
 	corpus_class_fields = {field_name: Column(String) for field_name in text_fields}
 	corpus_class_fields["id"] = Column(Integer, primary_key=True)
 	corpus_class_fields["__tablename__"] = input_table_name
@@ -99,22 +103,25 @@ def lexicon(input_table_name="set_input",
 	for doc in db_session.query(CorpusDoc).order_by("time").yield_per(128):
 		# Getting texts from document.
 		texts = [getattr(doc, text_field) for text_field in text_fields]
+		tokenizer = make_tokenizer()
 		# Use this set to account terms which were already
 		# occurred in the given document to not count them
 		# twice.
 		doc_occurrences = set()
 		# Extracting terms from texts.
 		for text in texts:
-			terms = tokenize(text)
-			for term in terms:
-				if term not in terms_dict:
-					lex_term = LexiconTerm(term)
-					terms_dict[term] = lex_term
+			terms = tokenizer.tokenize(text)
+			tagged_terms = assign_tags(terms)
+			for term, tag in tagged_terms:
+				term_id = term + tag
+				if term_id not in terms_dict:
+					lex_term = LexiconTerm(term, tag)
+					terms_dict[term_id] = lex_term
 				else:
-					lex_term = terms_dict[term]
+					lex_term = terms_dict[term_id]
 				lex_term.tfreq += 1
-				if term not in doc_occurrences:
-					doc_occurrences.add(term)
+				if term_id not in doc_occurrences:
+					doc_occurrences.add(term_id)
 					lex_term.dfreq += 1
 		if verbose:
 			loop_steps += 1
@@ -139,24 +146,37 @@ def lexicon(input_table_name="set_input",
 		lex_term.id = i
 		i += 1
 		lex_terms.append(lex_term)
+#		lex_terms.append({
+#			"id": lex_term.id,
+#			"term": lex_term.term,
+#			"tag": lex_term.tag,
+#			"tfreq": lex_term.tfreq,
+#			"dfreq": lex_term.dfreq,
+#			"rfreq": lex_term.rfreq,
+#		})
+	
+	
 	logging.debug("saving terms")
 	if csv_output:
 		csv_file = open(csv_output, "w")
-		cols = "id,term,tfreq,dfreq,rfreq\n"
-		pattern = u"{0},\"{1}\",{2},{3},{4:0.16f}\n"
+		cols = "id,term,tag,tfreq,dfreq,rfreq\n"
+		pattern = u"{0},\"{1}\",\"{2}\",{3},{4},{5:0.16f}\n"
 		csv_file.write(cols)
-		for l_term in lex_terms:
+		for lex_term in lex_terms:
 			csv_file.write(
 				pattern.format(
-					l_term.id,
-					l_term.term,
-					l_term.tfreq,
-					l_term.dfreq,
-					l_term.rfreq
+					lex_term.id,
+					lex_term.term.replace("\"","''"),
+					lex_term.tag.replace("\"","''"),
+					lex_term.tfreq,
+					lex_term.dfreq,
+					lex_term.rfreq
 				).encode("UTF-8")
 			)
 		csv_file.close()
 	else:
+#		db_conn = db_engine.connect()
+#		db_conn.execute(LexiconTerm.__table__.insert(), lex_terms)
 		db_session.add_all(lex_terms)
 		db_session.commit()
 		db_session.flush()
--- analyzer/nlp.py
+++ analyzer/nlp.py
@@ -1,34 +1,49 @@
 # -*- coding: utf-8 -*-
 
-from nltk import WordNetLemmatizer
+#from nltk import WordNetLemmatizer
+#from nltk import RegexpTokenizer
+from nltk import WordPunctTokenizer
+from nltk import pos_tag
 
-import re
+pattern = r'''(?x)[A-Z]\.)+
+|\w+(-\w+)*
+|\$?\d+(\.\d+)?%?
+|\.\.\.
+|[][.,;\"'?():-_`]'''
+def make_tokenizer():
+	return WordPunctTokenizer()
 
-refining_patterns = [("\n?.*\[[0-9]+\]: .*\n?", ""),
-	("\[[0-9]+\]", ""),
-	("[^a-zA-Z0-9-\']", " "),
-	("  *", " ")]
+def assign_tags(tokens):
+	return [(token.lower(), tag) for token, tag in pos_tag(tokens)]
 
-refining_regexes = [re.compile(p) for p, r in refining_patterns]
-tokenizer_pattern = re.compile(u"\W+")
-stemmer = WordNetLemmatizer()
-empty_string = re.compile("^\s*$")
 
-def clean_up(text):
-	for i in xrange(len(refining_regexes)):
-		r = refining_regexes[i]
-		_, replace = refining_patterns[i]
-		text = r.sub(replace, text)
-	text = text.lower()
-	return text
-
-def stem(token):
-	return stemmer.lemmatize(token)
-
-def filter_test(term):
-	return not empty_string.match(term)
-
-def tokenize(text):
-	text = clean_up(text)
-	words = tokenizer_pattern.split(text)
-	return [stemmer.lemmatize(w) for w in words if filter_test(w)]
+#import re
+#
+#refining_patterns = [("\n?.*\[[0-9]+\]: .*\n?", ""),
+#	("\[[0-9]+\]", ""),
+#	("[^a-zA-Z0-9-\']", " "),
+#	("  *", " ")]
+#
+#refining_regexes = [re.compile(p) for p, r in refining_patterns]
+#tokenizer_pattern = re.compile(u"\W+")
+#stemmer = WordNetLemmatizer()
+#empty_string = re.compile("^\s*$")
+#
+#def clean_up(text):
+#	for i in xrange(len(refining_regexes)):
+#		r = refining_regexes[i]
+#		_, replace = refining_patterns[i]
+#		text = r.sub(replace, text)
+#	text = text.lower()
+#	return text
+#
+#def stem(token):
+#	return stemmer.lemmatize(token)
+#
+#def filter_test(term):
+#	return not empty_string.match(term)
+#
+#def tokenize(text):
+#	text = clean_up(text)
+#	words = tokenizer_pattern.split(text)
+#	return [stemmer.lemmatize(w) for w in words if filter_test(w)]
