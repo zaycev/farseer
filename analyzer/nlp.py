d6746b031ff3f5026d72814bb526be6f2a5d50a3
Vladimir Zaytsev <vladimir@zvm.me>
2012-04-03 17:42:25 +0600

    added tagger maker

--- analyzer/nlp.py
+++ analyzer/nlp.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from analyzer.tagger import make_tagger
+
 from nltk import TreebankWordTokenizer
 from nltk.data import load
 from nltk.tag import _POS_TAGGER
@@ -7,30 +9,37 @@ from nltk.chunk import _MULTICLASS_NE_CHUNKER
 from nltk.tokenize import PunktSentenceTokenizer
 
 import logging
+import pickle
 import gc
 
 UTIL = None
 term_cache = None
-
 
 class NlpUtil(object):
 
 	def __init__(self):
 		self.wrd_tokenizer = TreebankWordTokenizer()
 		self.snt_tokenizer = PunktSentenceTokenizer()
-		self.pos_tagger = load(_POS_TAGGER)
+		self.pos_tagger = pickle.load(open("tagger.pickle", "r"))
 		self.ner_tagger = load(_MULTICLASS_NE_CHUNKER)
 
 	def wrd_tokenize(self, text):
-		# snts = self.snt_tokenizer.tokenize(text)
-		# word_tokens = []
-		# for s in snts:
-		# 	word_tokens.extend(self.wrd_tokenizer.tokenize(s))
-		# return word_tokens
-		return self.wrd_tokenizer.tokenize(text)
+		snts = self.snt_tokenizer.tokenize(text)
+		word_tokens = []
+		for s in snts:
+			word_tokens.extend(self.wrd_tokenizer.tokenize(s))
+		return word_tokens
+		# return self.wrd_tokenizer.tokenize(text)
 
 	def pos_tag(self, tokens):
-		return self.pos_tagger.tag(tokens)
+		tagged = self.pos_tagger.tag(tokens)
+		new_tagged = []
+		for word, tag in tagged:
+			if tag is None:
+				new_tagged.append((word, "?"))
+			else:
+				new_tagged.append((word, tag))
+		return new_tagged
 
 	def ner_tag(self, tagged_tokens):
 		return self.ner_tagger.parse(tagged_tokens)
---
+++ analyzer/tagger.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+import nltk.corpus
+import nltk.tag
+import itertools
+from nltk.corpus import brown, conll2000, treebank
+from nltk.tag import brill
+
+def backoff_tagger(tagged_sents, tagger_classes, backoff=None):
+	if not backoff:
+		backoff = tagger_classes[0](tagged_sents)
+		del tagger_classes[0]
+	for cls in tagger_classes:
+		tagger = cls(tagged_sents, backoff=backoff)
+		backoff = tagger
+	return backoff
+
+def make_tagger():
+	brown_reviews = brown.tagged_sents(categories=['reviews'])[1000:]
+	brown_lore = brown.tagged_sents(categories=['lore'])[1000:]
+	conll_train = conll2000.tagged_sents('train.txt')[2000:]
+	treebank_train = treebank.tagged_sents()[2000:]
+	train_sets = list(itertools.chain(
+		brown_reviews,
+		brown_lore,
+		conll_train,
+		treebank_train
+	))
+	tagger_classes = [
+		nltk.tag.AffixTagger,
+		nltk.tag.UnigramTagger,
+		nltk.tag.BigramTagger,
+		nltk.tag.TrigramTagger,
+	]
+	word_patterns = [
+		(r'^-?[0-9]+(.[0-9]+)?$', 'CD'),
+		(r'.*ould$', 'MD'),
+		(r'.*ing$', 'VBG'),
+		(r'.*ed$', 'VBD'),
+		(r'.*ness$', 'NN'),
+		(r'.*ment$', 'NN'),
+		(r'.*ful$', 'JJ'),
+		(r'.*ious$', 'JJ'),
+		(r'.*ble$', 'JJ'),
+		(r'.*ic$', 'JJ'),
+		(r'.*ive$', 'JJ'),
+		(r'.*ic$', 'JJ'),
+		(r'.*est$', 'JJ'),
+		(r'^a$', 'PREP'),
+	]
+	raubt_tagger = backoff_tagger(
+		train_sets,
+		tagger_classes,
+		backoff=nltk.tag.RegexpTagger(word_patterns),
+	)
+	return raubt_tagger
+
+	templates = [
+		brill.SymmetricProximateTokensTemplate(brill.ProximateTagsRule, (1,1)),
+		brill.SymmetricProximateTokensTemplate(brill.ProximateTagsRule, (2,2)),
+		brill.SymmetricProximateTokensTemplate(brill.ProximateTagsRule, (1,2)),
+		brill.SymmetricProximateTokensTemplate(brill.ProximateTagsRule, (1,3)),
+		brill.SymmetricProximateTokensTemplate(brill.ProximateWordsRule, (1,1)),
+		brill.SymmetricProximateTokensTemplate(brill.ProximateWordsRule, (2,2)),
+		brill.SymmetricProximateTokensTemplate(brill.ProximateWordsRule, (1,2)),
+		brill.SymmetricProximateTokensTemplate(brill.ProximateWordsRule, (1,3)),
+		brill.ProximateTokensTemplate(brill.ProximateTagsRule, (-1, -1), (1,1)),
+		brill.ProximateTokensTemplate(brill.ProximateWordsRule, (-1, -1), (1,1)),
+	]
+
+	trainer = brill.FastBrillTaggerTrainer(raubt_tagger, templates)
+	braubt_tagger = trainer.train(train_sets, max_rules=100, min_score=3)
+
+	return braubt_tagger
+
